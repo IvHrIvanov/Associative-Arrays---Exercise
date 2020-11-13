056bf94eb92c24f2ad0dee55b9ff1bc4700b240d
IvanHrIvanov <vinko96@abv.bg>
2020-11-13 20:08:46 +0200

    Add project files.

---
+++ 02. A Miner Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resours = Console.ReadLine();
+
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            while (resours != "stop")
+            {
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!output.ContainsKey(resours))
+                {
+                    output.Add(resours, quantity);
+                }
+                else
+                {
+                    output[resours] += quantity;
+                }
+
+                resours = Console.ReadLine();
+            }
+            foreach (var item in output)
+            {
+
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+
+            }
+        }
+    }
+}
