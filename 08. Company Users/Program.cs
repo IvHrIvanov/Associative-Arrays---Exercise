102b00e03a0f726dd01cda7d799c56fa7dc5dd29
IvanHrIvanov <vinko96@abv.bg>
2020-11-16 23:38:34 +0200

    Add

---
+++ 08. Company Users/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(" -> ");
+
+            Dictionary<string, List<string>> users = new Dictionary<string, List<string>>();
+
+            while (input[0] != "End")
+            {
+                string company = input[0];
+                string userId = input[1];
+
+                if (!users.ContainsKey(company))
+                {
+                    users.Add(company, new List<string>());
+                }
+                if(!users[company].Contains(userId))
+                {
+                    users[company].Add(userId);
+                }
+
+                input = Console.ReadLine().Split(" -> ");
+            }
+
+            foreach (var currentCompany in users.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{currentCompany.Key}");
+                foreach (var item in currentCompany.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+
+        }
+    }
+}
